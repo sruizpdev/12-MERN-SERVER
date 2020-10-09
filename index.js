9ea8805b545a3130d51feae5d61c8b9cb259a02b
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-10-09 12:41:35 +0200

    adding mongodb

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hola!!!');
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+connectDB();
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
